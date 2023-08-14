fd9df8144b8f9b9b36b7be55e610e449d0d4fe42
mauricioPReis <mauricioreis.tads@gmail.com>
2023-08-13 21:35:26 -0300

    Icluisação do doSWAGGER

--- src/main/java/fadesp/paymentrestapi/PaymentRestApiApplication.java
+++ src/main/java/fadesp/paymentrestapi/PaymentRestApiApplication.java
@@ -2,12 +2,14 @@ package fadesp.paymentrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PaymentRestApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PaymentRestApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentRestApiApplication.class, args);
+    }
 
 }
